81a012018b37e8e0e09752bdae2049c3e592aaa4
wergut <web-style.programming@mail.ru>
2023-08-17 17:07:00 +0500

    css

--- js/quiz/order.js
+++ js/quiz/order.js
@@ -91,3 +91,100 @@ document.addEventListener('DOMContentLoaded', function () {
         hiddenDesc.style.display = 'block';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const secretCodeInput = document.querySelector('.secret-code');
+    const toggleCodeBtn = document.querySelector('.toggle-code-btn');
+
+    toggleCodeBtn.addEventListener('click', function () {
+        if (secretCodeInput.type === 'password') {
+            secretCodeInput.type = 'text';
+            toggleCodeBtn.classList.add('active');
+        } else {
+            secretCodeInput.type = 'password';
+            toggleCodeBtn.classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cardNumInput = document.querySelector('.card-num');
+    const nameInput = document.querySelector('.name');
+    const dateInput = document.querySelector('.date');
+    const secretCodeInput = document.querySelector('.secret-code');
+    const submitButton = document.querySelector('.submit-button');
+    const errorMessage = document.querySelector('.error-message');
+
+    submitButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Проверка на валидность номера карты (проверка на число и длину)
+        const cardNumValue = cardNumInput.value.replace(/\s/g, ''); // Убираем пробелы
+        if (!/^\d{16}$/.test(cardNumValue)) {
+            errorMessage.textContent = 'Please enter a valid card number (16 digits).';
+            return;
+        }
+
+        // Проверка на заполнение имени
+        if (nameInput.value.trim() === '') {
+            errorMessage.textContent = 'Please enter your name.';
+            return;
+        }
+
+        // Проверка на валидность даты (проверка на формат)
+        if (!/^\d{2}\/\d{2}$/.test(dateInput.value)) {
+            errorMessage.textContent = 'Please enter a valid date (MM/YY).';
+            return;
+        }
+
+        // Проверка на валидность кода безопасности (проверка на число и длину)
+        if (!/^\d{3}$/.test(secretCodeInput.value)) {
+            errorMessage.textContent = 'Please enter a valid security code (3 digits).';
+            return;
+        }
+
+        // Все поля заполнены корректно, можно отправить данные на сервер или выполнить другую логику
+        errorMessage.textContent = '';
+        alert('Payment successful!');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modalButtons = document.querySelectorAll('.open-modal-dialog'),
+        overlay      = document.querySelector('body'),
+        closeButtons = document.querySelectorAll('.modal-dialog .modal-close');
+
+    /* open modal*/
+    modalButtons.forEach(function(modalBtn){
+        modalBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            var modalId = this.getAttribute('data-src'),
+                modalElem = document.querySelector('.modal-dialog.'+modalId);
+            overlay.classList.add('modal-open');
+            modalElem.style.display = "block";
+            modalElem.classList.add('modal-opening');
+        }); // end click
+    }); // end foreach
+
+
+    /* close modal */
+    closeButtons.forEach(function(closeBtn) {
+        closeBtn.addEventListener('click', function (e) {
+            this.closest('.modal-dialog').style.display = "none";
+            overlay.classList.remove('modal-open');
+            this.closest('.modal-dialog').classList.remove('modal-opening');
+        })
+    });
+
+    document.querySelectorAll('.modal-dialog').forEach(function(item) {
+        item.addEventListener('click', function (e) {
+            if(e.target !== e.currentTarget) {
+                return
+            } else {
+                this.style.display = "none";
+                this.classList.remove('modal-opening');
+            }
+        })
+    });
+});
